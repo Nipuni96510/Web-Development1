a4d1b37808805a7671d8629b097d3d96450c0d58
Nipunu96510 <96510@fhss.sjp.ac.lk>
2022-03-01 00:58:24 +0700

    my web page

---
+++ src/yourdata.js
@@ -0,0 +1,196 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+import aboutImage from "./images/NIPUNI.png"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Nipuni Dissanayaka",
+  headerTagline: [
+    //Line 1 For Header
+    "Building Digital",
+    //Line 2 For Header
+    "Designs,projects",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "This includes creative designs,projects and my ecperiences with mavis.",
+
+  //Contact Email
+  contactEmail: "dissanayakanipuni329@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "1.Post creating(upcoming)", //Project Title - Add Your Project Title Here
+      para:
+        "Post creation for the Japura Vikings Mentora project,which is expected to take place in March.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "2.Health Management System", //Project Title - Add Your Project Title Here
+      para:
+        "This project was done by me as a hospital health management system.This was created using the Python language and was designed to enable users to maintain their services through an online system.(University First Year Project)", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "3.Payroll System", //Project Title - Add Your Project Title Here
+      para:
+        "This projec was done by me as a payroll system in a service organization.This was created using the Java language and designed so that users can maintain their payroll services.(University Second Project)", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "4.Crypto Currency App", //Project Title - Add Your Project Title Here
+      para:
+        "I created this application based on react-native.This application was created because crypto currency is widely used today and it should be safe for users.(University third year first semester project)", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "5.Web Page Creation", //Project Title - Add Your Project Title Here
+      para:
+        "I used HTML and CSS to create webpages.This web page was created for my MGIT project.(University third year first semester MGIT project)", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "6.Illustrator  Arts", //Project Title - Add Your Project Title Here
+      para:
+        "Here I did some Arts using Adobe Illustrator.Those are just creations made because as my hobby.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "I'm Nipuni Dissanayaka,a third year student ICT Bsc(Hons) undergraduate student in University of Sri Jayewardenepura.",
+  aboutParaTwo:
+    "I like everything related to technology.I thrive on new experiences and stepping out of my comfort zone to learn what my strengths and weakness are,not only in university life but in all aspects of lifeI also work to develop my abilities.Eager to learn everything possible,I am currently studing for a Diploma in HRM and a Diploma in Tamil in addition to my First Degree",
+  aboutParaThree:
+    "Most prefer multimedia subjects such as photoshop,illustator; and these days working on webtoon design.",
+  aboutImage:aboutImage,
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "Creating web pages using HTML ",
+    },
+    {
+      img: cssIcon,
+      para:
+        "Creating web pages using CSS",
+    },
+    {
+      img: jsIcon,
+      para:
+        "Web development can be done using java script",
+    },
+    {
+      img: reactIcon,
+      para:
+        "Create user interfaces with react-native",
+    },
+    {
+      img: designIcon,
+      para:
+        "Designing using AI and Photoshop",
+    },
+    {
+      img: codeIcon,
+      para:
+        "Coding using python,Java language",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/Nipuni96510" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    {
+      img: dribbbleIcon,
+      url: "https://dribbble.com/chetanverma",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
